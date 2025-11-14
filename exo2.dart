0a4bbf7febfc8c96d3b8edb6fc9be0156715cc2a
Arona Barry <300141429@monboreal.ca>
2025-11-14 18:16:46 -0500

    utilisation de formatException

---
+++ exo2.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print(" Veuillez Entrer votre âge SVP : ");
+
+  String? input = stdin.readLineSync();
+
+  try {
+    int age = int.parse(input!);
+    print("Votre âge est : $age");
+  } on FormatException {
+    print(
+      "Format invalide : veuillez entrer un nombre le format n'est pas valide!",
+    );
+  }
+}
